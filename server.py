05533b04b0c08e4e03ad962222f9d7f807f948ff
Lavanya-4321 <116573812+Lavanya-4321@users.noreply.github.com>
2025-07-07 19:04:33 +0530

    Update server.py

--- server.py
+++ server.py
@@ -20,6 +20,7 @@ def serve_js():
 
 @app.route('/classify_image', methods=['GET', 'POST'])
 def classify_image():
+    util.load_saved_artifacts()
     image_data = request.form['image_data']
 
     response = jsonify(util.classify_image(image_data))
@@ -30,5 +31,5 @@ def classify_image():
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Sports Celebrity Image Classification")
-    util.load_saved_artifacts()
+    
     app.run(port=5000)
